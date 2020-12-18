49da7d7e685841ef8a081f5ae651f6de12d230d6
Egas099 <egas099@gmail.com>
2020-12-18 15:02:01 +0300

    Task completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import Loader from 'components/loader'
+import UsersList from 'components/usersList'
+import styles from 'main.css'
+import { Context } from 'context'
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [usersExist, setUsetsExist] = useState(false);
+  const [fileName, setFileName] = useState("");
+
+  const importUsers = users => {
+    setUsers(users);
+    setUsetsExist(true);
+  }
+  const removeUser = index => {
+    setUsers(users.filter(user => {
+      return users.indexOf(user) !== index;
+    }));
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Context.Provider value={{
+      removeUser, importUsers, setFileName
+    }}>
+      <div className="App" style={styles}>
+        {
+          function () {
+            if (usersExist) {
+              return [
+                <p key="fileName" className="fileName">{fileName}</p>,
+                <UsersList key="userList" users={users} />
+              ];
+            } else {
+              return <Loader />;
+            }
+          }()
+        }
+      </div>
+    </Context.Provider>
   );
 }
 
